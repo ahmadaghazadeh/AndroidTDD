11375cc378d554604fc11d4750d5a33b192fbb12
Ahmad Aghazadeh PC <ahmad.aghazadeh@outlook.com>
2023-03-03 10:34:34 +0330

    Configure the project structure

---
+++ src/app/src/main/java/com/arad/androidtdd/Playlist.kt
@@ -0,0 +1,4 @@
+package com.arad.androidtdd
+
+class Playlist {
+}
---
+++ src/app/src/main/java/com/arad/androidtdd/PlaylistFragment.kt
@@ -0,0 +1,51 @@
+package com.arad.androidtdd
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.arad.androidtdd.placeholder.PlaceholderContent
+
+
+class PlaylistFragment : Fragment() {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyPlaylistRecyclerViewAdapter(PlaceholderContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+
+        @JvmStatic
+        fun newInstance( ) =
+            PlaylistFragment().apply {
+
+            }
+    }
+}
